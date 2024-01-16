816d875411afbd28869fbe3b700fe75a9a222257
Jose Navarro <jodanape89@hotmail.com>
2024-01-16 18:48:28 -0500

    agregadoo

---
+++ si/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jodan
+ *
+ */
+module si {
+}
---
+++ si/src/si/no.java
@@ -0,0 +1,10 @@
+package si;
+
+public class no {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
